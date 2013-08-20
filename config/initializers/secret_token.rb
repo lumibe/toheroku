84ee2b1850285f8beb69e416f6d344f03e5eb5d5
Luis Miguel <lumibe@gmail.com>
2013-08-20 18:40:00 -0500

    first commit pa variar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toheroku::Application.config.secret_token = 'c91d7819161144fa7b656b9d7fb70f793d4d9c4e0b2bf909ee2eaef9c729a5d46e67359518a5fb11fe8ffd808bbd820a4b8eea9b3db828914ca471012ba34ba0'
